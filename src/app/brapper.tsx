20e2b902191015d7bc0177bd6e386740cffb4d00
Radek Sedlák <sedlak.r@gmail.com>
2021-09-15 15:48:53 +0200

    BReact createElement simple case

--- src/app/brapper.tsx
+++ src/app/brapper.tsx
@@ -1,8 +1,44 @@
-import React from "react";
-import * as Breact from "breact";
+import * as React from "breact";
+import * as b from "breact";
+
 
 export function BobrilReactIntagration() {
 	return (
-		<div>{Breact.breact}</div>
+		<>
+			<div>Bobril react wrapper</div>
+			<BReact/>
+		</>
 	);
+}
+
+const style = b.styleDef({
+	color: 'red',
+	borderLeft: '2px solid yellow',
+	paddingLeft: '14px',
+});
+
+const style1 = b.styleDef({
+	color: 'blue',
+	borderLeft: '2px solid yellow',
+	paddingLeft: '14px',
+});
+
+const style2 = b.styleDef({
+	color: '#9a409a',
+	paddingLeft: '14px',
+});
+
+export function BReact() {
+	const simpleDiv = b.styledDiv('hello');
+	const styledDiv = b.styledDiv('world', style1);
+	const styledDiv2 = b.styledDiv('nazdar', style2);
+	const styledDiv2copy = b.styledDiv('nazdar2', style2);
+
+	return (<>
+		{simpleDiv}
+		{styledDiv}
+		{styledDiv2}
+		{styledDiv2copy}
+		<div style={style}>cau</div>
+	</>);
 }
--- src/breact/b.tsx
+++ src/breact/b.tsx
@@ -1,1 +1,51 @@
-export const breact = "Bobril react wrapper";
+import * as React from 'react';
+import { ReactNode } from 'react';
+import { CSSObject } from 'styled-components';
+import { createUseStyles, Styles } from 'react-jss';
+import { JssStyle } from "jss";
+
+let i = 0;
+
+function getStyleId() {
+	return i++;
+}
+
+export type BRStyles = {
+	css: JssStyle;
+	id: string
+}
+
+export function styleDef(cssLike: CSSObject): BRStyles {
+	return {
+		css: cssLike,
+		id: `${getStyleId()}`,
+	};
+}
+
+export function styledDiv(children: ReactNode | undefined, css?: BRStyles): ReactNode {
+	return createElement('div', {style: css} as any, children);
+}
+
+let styleMap = new Map();
+
+export function createElement(element: string, props: any, ...children: any) {
+	let custProps: any = {};
+	if (props?.style) {
+		const style: BRStyles = props.style;
+		if (styleMap.has(style.id)) {
+			custProps.className = styleMap.get(style.id);
+		} else {
+			let createdSTyle = createUseStyles({
+				_d: props.style.css as Styles,
+			});
+			let clz = createdSTyle();
+			let className = clz._d;
+			custProps.className = className;
+			styleMap.set(style.id, className);
+		}
+	}
+
+	return React.createElement(element, custProps, ...children);
+}
+
+export const Fragment = React.Fragment;
