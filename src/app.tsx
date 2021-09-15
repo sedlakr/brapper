c07e2f9a26f3e01bbeff680f45325cad688ce8c1
Radek Sedlák <sedlak.r@gmail.com>
2021-09-15 15:11:10 +0200

    init app

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { BobrilReactIntagration } from "./brapper";
+
+export function App() {
+	return <>
+		<BobrilReactIntagration/>
+	</>
+}
---
+++ src/brapper.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function BobrilReactIntagration() {
+	return (
+		<div>BReact!</div>
+	);
+}
