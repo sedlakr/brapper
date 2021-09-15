fbda290a898cf30e75d54a9a7fe31c96570f029d
Radek Sedlák <sedlak.r@gmail.com>
2021-09-15 15:48:22 +0200

    styled components for helping types

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,23 @@
 import * as React from "react";
 import { BobrilReactIntagration } from "./brapper";
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`
 
 export function App() {
-	return <>
-		<BobrilReactIntagration/>
-	</>
+	return (
+		<>
+			<GlobalStyle/>
+			<div style={{background: "#ccc",height: "100vh", padding: "10px"}}>
+
+				<BobrilReactIntagration/>
+			</div>
+		</>
+	);
 }
